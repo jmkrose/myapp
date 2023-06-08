5970de876f5fb7979737975cf3af2baab4c6e045
AC931756\장미경 <jmkrose0704@gmail.com>
2023-06-08 15:47:58 +0900

    연결 페이지 변경

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://naver.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          네이버
         </a>
       </header>
     </div>
